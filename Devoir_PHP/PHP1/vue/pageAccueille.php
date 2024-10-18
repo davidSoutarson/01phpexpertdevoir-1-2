8ac99ac1f016bd971b7223ed4fc391e6155484c5
davidSoutarson <triplaxs@gmail.com>
2024-10-18 16:59:51 +0200

    Commite-02 Test de la BancheDeTravaille

--- Devoir_PHP/PHP1/vue/pageAccueille.php
+++ Devoir_PHP/PHP1/vue/pageAccueille.php
@@ -1,4 +1,5 @@
 <?php
 echo "<p>Helo je suis la future page d'acueille du projet </p>";
+echo "<p> Teste BancheDeTravaille </p>";
 
 /* Commit-01. 18/10/2024  inisialisation du projet. Branche master. Commit-01 fonctionelle. Ne pas modifier avant. Avencer majeur verfier et fonctionlle */
